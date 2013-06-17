a2a66e35919fe5cf100db5da2a66605ce4867d78
Mayoto Yoshida <2getjp@gmail.com>
2013-06-17 12:15:52 +0900

    テストできるように

--- spec/routing/topics_routing_spec.rb
+++ spec/routing/topics_routing_spec.rb
@@ -4,31 +4,31 @@ describe TopicsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/topics").should route_to("topics#index")
+      get("/").should route_to("topics#index")
     end
 
     it "routes to #new" do
-      get("/topics/new").should route_to("topics#new")
+      get("/new").should route_to("topics#new")
     end
 
     it "routes to #show" do
-      get("/topics/1").should route_to("topics#show", :id => "1")
+      get("/1").should route_to("topics#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/topics/1/edit").should route_to("topics#edit", :id => "1")
+      get("/1/edit").should route_to("topics#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/topics").should route_to("topics#create")
+      post("/").should route_to("topics#create")
     end
 
     it "routes to #update" do
-      put("/topics/1").should route_to("topics#update", :id => "1")
+      put("/1").should route_to("topics#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/topics/1").should route_to("topics#destroy", :id => "1")
+      delete("/1").should route_to("topics#destroy", :id => "1")
     end
 
   end
