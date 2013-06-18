1256029d46f636993fe98c4034f878d50a3c9d4d
Mayoto Yoshida <2getjp@gmail.com>
2013-06-18 16:10:38 +0900

    Add validate and topic order created_at desc

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -23,7 +23,7 @@ describe TopicsController do
   # This should return the minimal set of attributes required to create a valid
   # Topic. As you add validations to Topic, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "subject" => "MyString" } }
+  let(:valid_attributes) { { "subject" => "MyString", "name" => "MyName", "body" => "MyBody\nMyBody2" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
