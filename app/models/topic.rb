7db70813a77f40ff87956934c7bc19a03d05a43c
Mayoto Yoshida <2getjp@gmail.com>
2013-06-19 17:44:48 +0900

    コメントをPOSTできるようにした

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,9 @@
 class Topic < ActiveRecord::Base
+  has_many :comments
+
   validates :subject, presence: true, length: { maximum: 140 }
   validates :name, presence: true, length: { maximum: 50 }
   validates :body, presence: true
 
-  paginates_per 5
+  default_scope { order 'created_at DESC' }
 end
