64a7d27d7fbecc44e42ed1bde417f65406479df8
Mayoto Yoshida <2getjp@gmail.com>
2013-06-21 16:19:17 +0900

    Add validate to comment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   has_many :comments
 
   validates :subject, presence: true, length: { maximum: 140 }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 20 }
   validates :body, presence: true
 
   paginates_per 5
