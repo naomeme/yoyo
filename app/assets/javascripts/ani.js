e58129a62718ff34b981ae50f738fa7a3744cd5f
nao <nao@seppuku.local>
2013-06-22 20:27:10 +0900

    タイトルにリンク、js速度変更、application.cssの中身をtopic.css.scssに移動

--- app/assets/javascripts/ani.js
+++ app/assets/javascripts/ani.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 	var scrollSpeed = 1;
-	var imgWidth = 5;
+	var imgWidth = 2;
 	var posX = 0;
 	setInterval(function(){
 		if (posX >= imgWidth) posX= 0;
 		posX += scrollSpeed;
 		$('body').css("background-position",posX+"px 0px");
-	}, 1);
+	}, 50);
 });
